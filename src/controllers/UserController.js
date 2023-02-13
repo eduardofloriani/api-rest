09a42b5d13bc1588ca64c6bba7e22f05836227ad
eduardofloriani <eeduardofloriani@gmail.com>
2023-02-13 11:05:12 -0300

    user crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,7 @@
 import User from '../models/User';
 
 class UserController {
+  // Store, Create
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
@@ -8,6 +9,57 @@ class UserController {
     } catch (e) {
       res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
+  }
+
+  // Index - all users
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) { return res.json(null); }
+  }
+
+  // Show - 1 user
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) { return res.json(null); }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ['Id não enviado.'] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({ errors: ['Usuário não existe.'] });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) { return res.json(null); }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ erros: ['Id não enviado.'] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({ errors: ['Usuário não encontrado.'] });
+      }
+
+      await user.destroy();
+      return res.json('Usuário deletado.');
+    } catch (e) { return res.json(null); }
   }
 }
 
