e4f1422f1c4f80056e9061cd81dc1f070a29c243
eduardofloriani <eeduardofloriani@gmail.com>
2023-02-14 20:47:39 -0300

    multer fix, handling uploaded files

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
-import multerConfig from '../config/multerConfig';
 import FotoController from '../controllers/FotoController';
 
-const upload = multer(multerConfig);
 const router = new Router();
 
-router.post('/', upload.single('foto'), FotoController.store);
+router.post('/', FotoController.store);
 
 export default router;
